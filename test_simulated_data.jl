dcb195f46f0852ece95e5ffc940ba3650155c985
yanshu <huangfeifei91@gmail.com>
2014-04-14 18:40:55 -0400

    test simulated data

--- test_simulated_data.jl
+++ test_simulated_data.jl
@@ -3,26 +3,23 @@ using PyCall
 @pyimport kplr
 #@pyimport matplotlib.pyplot as pl
 
-TIME = zeros(1000)
-for i=1:1000
+# generate a simulated TIME and FLUX array with period = 400, duration = 25, epoch = 50
+
+n = 10000	# total number of data points
+p = 400
+d = 25
+
+TIME = zeros(n)
+for i=1:n
 	TIME[i] += i
 end
-FLUX = randn(1000)*0.1
-for i=50:75
-	FLUX[i] += -1.+rand()*0.1
+FLUX = randn(n)*0.1
+e = [50+400*i for i=0:ifloor((n-50)/p)]		# all the starting time of transits 
+for e_idx = 1:length(e)
+	for i=e[e_idx]:e[e_idx]+d
+		FLUX[i] += -1.+rand()*0.1
+	end
 end
-FLUX[50] -= 0.5
-FLUX[75] -= 0.5
-for i=450:475
-	FLUX[i] += -1.+rand()*0.1
-end
-FLUX[450] -= 0.5
-FLUX[475] -= 0.5
-for i=850:875
-	FLUX[i] += -1.+rand()*0.1
-end
-FLUX[850] -= 0.5
-FLUX[875] -= 0.5
 
 #plot FLUX vs TIME
 #plotEverything(TIME,FLUX,"97.01")
